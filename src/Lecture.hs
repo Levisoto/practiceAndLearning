5ca105d398a50b0f9708eb3c6685aa7d0ef98fbf
Levisoto <levisoto.337@gmail.com>
2017-12-01 11:56:31 -0500

    Tutorial: Linear Regression completed

--- src/Lecture.hs
+++ src/Lecture.hs
@@ -1,16 +1,16 @@
 module Lecture where
 
-import Graphics.Rendering.Chart.Easy
-import Graphics.Rendering.Chart.Backend.Cairo
+-- import Graphics.Rendering.Chart.Easy
+-- import Graphics.Rendering.Chart.Backend.Cairo
 
-signal :: [Double] -> [(Double,Double)]
-signal xs = [ (x,(cos (x*3.14159/45) + 1) / 2 * (sin (x*3.14159/5))) | x <- xs ]
+-- signal :: [Double] -> [(Double,Double)]
+-- signal xs = [ (x,(cos (x*3.14159/45) + 1) / 2 * (sin (x*3.14159/5))) | x <- xs ]
 
-main = toFile def "example1_big.png" $ do
-    layout_title .= "Amplitude Modulation"
-    setColors [opaque blue, opaque red]
-    plot (line "am" [signal [0,(0.5)..400]])
-    plot (points "am points" (signal [0,3..400]))
+-- main = toFile def "example1_big.png" $ do
+--     layout_title .= "Amplitude Modulation"
+--     setColors [opaque blue, opaque red]
+--     plot (line "am" [signal [0,(0.5)..400]])
+--     plot (points "am points" (signal [0,3..400]))
 -----------------------------------------------------------------
 -----------------------------------------------------------------
 reqs  = client initio resps
--- src/LinearRegression.hs
+++ src/LinearRegression.hs
@@ -4,3 +4,44 @@ import Statistics.LinearRegression
 import qualified Data.Vector.Unboxed as U
 import Data.Decimal
 import Graphics.EasyPlot
+
+xs :: [Double]
+xs = [25.47,67.80,20.65,22.50,30.40,55.30,22.74] ++
+         [29.11,31.15,40.97,58.89,64.85,74.89,96.14] ++
+         [65.37,80.23,95.80,92.71,98.16,89.77]
+
+ys :: [Double]
+ys = [390.50,632.70,449.36,501.81,621.72,684.62,685.53] ++
+         [701.76,868.71,995.69,893.50,1240.48,1393.55] ++
+         [1826.70,2145.32,2295.27,2522.79,2931.69] ++
+         [2836.20,2456.30]
+
+-- Values from a list
+--
+varIndep  = U.fromList xs
+varDepend = U.fromList ys
+
+-- computing theta0 and theta1 : Y = t0 + b1.X
+
+(b0, b1) = linearRegression varIndep varDepend
+
+b0n = realFracToDecimal 3 b0
+b1n = realFracToDecimal 3 b1
+
+points = zip xs ys
+
+f x = b0 + b1*x
+g x = ((*b0).sin.(/b1)) x
+
+functionY = do 
+  plot (PNG "myThirdPlot.png") $ [
+                    Function2D
+                    [Title "Linear Regression", Color Red]
+                    [Range 0 140] (g)
+                    , Data2D
+                    [Title "Data", Color Blue]
+                    [] points
+                    , Function2D
+                    [Title "Linear Regression", Color Red]
+                    [Range 0 140] (f)
+                 ]
